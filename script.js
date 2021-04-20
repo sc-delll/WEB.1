a7c9961c08da6d6f42d63c32ee5e628b9acc5ec5
sc-delll <82901274+sc-delll@users.noreply.github.com>
2021-04-20 19:07:19 -0400

    added code for new music

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function music(){
   var music = new Audio('Chug Jug With You - Parody of American Boy (Number One Victory Royale).mp3');
  music.play();}
  function music2(){
-  var music = new Audio('SpongeBob theme song Earrape.mp3');
+  var music = new Audio('Rick Astley - Never Gonna Give You Up (BASS BOOSTED EAR RAPE).mp3');
  music.play();}
  function music3(){
-  var music = new Audio('MINE DIAMONDS miNECRAFT PARODY OF TAKE ON ME.mp3');
+  var music = new Audio('Dead Weight - Jack Stauber.mp3');
  music.play();}
